85350764180477e6f8f005ce8eb9b2150a50d483
m-88888888 <hachi8810@gmail.som>
2019-11-13 08:31:00 +0900

    [update]ユーザーViewのファイル名を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_161655) do
+ActiveRecord::Schema.define(version: 2019_11_04_125847) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "photo"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2019_10_30_161655) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "profile_image"
+    t.integer "gender"
+    t.integer "height"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
