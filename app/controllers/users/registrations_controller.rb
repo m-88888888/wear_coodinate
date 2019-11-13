fee6f1641fb249ba4833ba021748e2d8fa5f9a80
m-88888888 <hachi8810@gmail.som>
2019-11-13 23:56:55 +0900

    [add]userモデルにvalidation追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,7 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :name, :profile_image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :name, :profile_image, :gender, :height])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles, dependent: :destroy
-
   has_many :likes, dependent: :destroy
   # has_many :liking_articles, through: :likes, source: :article
 
-
   has_one_attached :image
   mount_uploader :profile_image, ProfileImageUploader
+
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :height, presence: true
+  
 end
