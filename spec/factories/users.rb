c48c1a76c7e349ec8d85b3a7737e9e9a0ce43527
m-88888888 <hachi8810@gmail.som>
2019-11-17 01:24:27 +0900

    [add] User Modelテストの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'test@test.com'}
+    password { 'password' }
+    password_confirmation { 'password'}
+    name { 'test_user' }
+    gender { 1 }
+    height { 180 }
   end
+
 end
