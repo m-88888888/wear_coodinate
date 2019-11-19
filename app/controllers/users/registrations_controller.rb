f16546b277b2b00b7a5b5fe869e8c4ff360b7eb6
m-88888888 <hachi8810@gmail.som>
2019-11-19 22:11:45 +0900

    [update]パスワードの入力なしにUserモデル編集できる

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -46,9 +46,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :encrypted_password, :name, :profile_image, :gender, :height])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
@@ -59,4 +59,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  # パスワードの入力をせずにUserモデルを更新する
+  def update_resource(resource, params)
+    if params[:password].present? && params[:password_confirmation].present?
+      resource.update_attributes(params)
+    else
+      resource.update_without_password(params)
+    end
+  end
 end
