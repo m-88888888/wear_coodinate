5d1fa06eff3aeb83c7dd54300dc56668416eebe7
m-88888888 <hachi8810@gmail.som>
2019-11-30 11:13:50 +0900

    [add]gearモデルにvalidationの追加

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -2,6 +2,8 @@ class Gear < ApplicationRecord
   mount_uploader :gear_image, GearImageUploader
   belongs_to :article, optional: true
 
-  validates :url, presence: true
-  validates :gear_image, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :brand, presence: true
+  validates :kind, presence: true
+  validates :model_year, presence: true, format: { with: /\A20\d{2}/, message: 'は「20XX」の形式で入力してください。' }
 end
