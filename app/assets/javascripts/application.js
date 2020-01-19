a9b10e117fc66d74a4ea3f2f446e75c99d97bb0e
m-88888888 <hachi8810@gmail.som>
2020-01-20 01:38:39 +0900

    #15[UPDATE]追加したフォームに画像プレビューが表示されるよう変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= reqyure jquery_nested_form
+//= require jquery_nested_form
 // require activestorage
 //= require_tree
 
@@ -27,45 +27,42 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }, false);
 
-  // gearフォームの画像プレビュー（37行目でエラー)
-  // function imgPreView(event, targetId) {
-  //   var reader = new FileReader();
-  //   var file = event.target.files[0];
-  //   var preview = document.getElementById(targetId);
-
-  //   reader.onload = function (event) {
-  //     // document.querySelector(targetId).src = reader.result;
-  //     document.querySelector("#preview" + targetId).src = reader.result;
-  //   };
-
-  //   reader.readAsDataURL(event.target.files[0]);
-  // }
-
-  var index = 1;
-  var fuga = '#preview';
-
-  document.getElementById('add-form').addEventListener("click", function () {
-    index += 1;
-    window.onload = hogehoge.bind(this, index); // 2
+  document.getElementById('article_gears_attributes_0_gear_image').addEventListener("change", function (evt) {
+    var file = evt.target.files;
+    var reader = new FileReader();
+    reader.readAsDataURL(file[0]);
+    reader.onload = function () {
+      document.querySelector('#preview').src = reader.result;
+    }
   }, false);
 
-  // 関数①
-  windows.nested_form_fields.bind_nested_forms_links = ()
-  function hogehoge(index) {
-    for (var i = 0; i < index; i++) {
-      (function (j) {
-        fuga += j.toFixed();
-        var id = 'article_gears_attributes_' + j + '_gear_image';
-        document.getElementById(id).addEventListener("change", function (evt) {
-          var file = evt.target.files;
-          var reader = new FileReader();
-          reader.readAsDataURL(file[0]);
-          reader.onload = function () {
-            document.querySelector(fuga).src = reader.result;
-          }
-        }, false);
-      }(i));
+});
+
+$(document).on('nested:fieldAdded', function (event) {
+  var field = event.field;
+  var addField = field.find('.add_nested_fields');
+  addField.click();
+});
+
+$(function() {
+
+  function setImagePreview() {
+    const targets = document.getElementsByClassName('image_preview')
+    for (let i = 0; i < targets.length; i++) {
+      targets[i].addEventListener("change", function(evt) {
+        var file = evt.target.files;
+        var reader = new FileReader();
+        reader.readAsDataURL(file[0]);
+        reader.onload = function () {
+          var preview_list = document.querySelectorAll('.gear_image')
+          preview_list[i].src = reader.result;
+        }
+      }, false);
     }
-  };
-  // window.onload = hogehoge(index);
+  }
+
+  $(document).on('nested:fieldAdded', function (event) {
+      setImagePreview();
+  });
+  
 });
